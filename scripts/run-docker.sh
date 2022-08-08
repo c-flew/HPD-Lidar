b5f2b206b56a062ef92b225eb2f7c7277e8c77ea
Christian Flewelling <cflew@tutanota.com>
2022-08-08 21:26:16 +0000

    fixed run script

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -8,12 +8,12 @@ getdevice() {
     idP=${1#*:}
     for path in `find /sys/devices/ -name idVendor | sed 's/idVendor$//'`; do
         if grep -q $idV $path/idVendor && grep -q $idP $path/idProduct; then
-            find $path -name 'device' | sed 's/device$//'
+	    find $path -name 'device' | rev | cut -d / -f 2 | rev
         fi
     done
 }
 
 device=/dev/$(getdevice 10c4:ea60)
+ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 
-# performance over security /shrug
-exec podman --runtime $(which crun) run -a -P --privledged --storage-driver=overlayfs --network host --device $device --rm lidar
+exec podman --runtime $(which crun) run --network host --device $device -e ROS_IP=$ip -it --rm lidar $@
